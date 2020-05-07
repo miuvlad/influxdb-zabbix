f26f2aa3e8ba918c97867143ee06675a9bc79787
Miu Vlad-Cosmin <miuvlad@gmail.com>
2020-05-07 06:48:52 -0400

    Add support for mysql history_text and history_str tables

--- input/mysql.go
+++ input/mysql.go
@@ -8,6 +8,10 @@ func mySQL(tablename string) string {
 		return mysqlHistory
 	case "history_uint":
 		return mysqlHistoryUInt
+	case "history_str":
+		return mysqlHistoryStr
+	case "history_text":
+		return mysqlHistoryText
 	case "trends":
 		return mysqlTrends
 	case "trends_uint":
@@ -164,6 +168,82 @@ replace(replace(CASE
 || ' ' || CAST((his.clock * 1000. + his.ns DIV 1000000.) as char) as INLINE
 ,  CAST((his.clock * 1000. + his.ns DIV 1000000.) as char) as clock
 FROM history_uint his
+INNER JOIN items ite on ite.itemid = his.itemid
+INNER JOIN hosts hos on hos.hostid = ite.hostid
+INNER JOIN hosts_groups hg on hg.hostid = hos.hostid
+INNER JOIN hstgrp grp on grp.groupid = hg.groupid
+WHERE grp.internal=0
+   AND his.clock > ##STARTDATE##
+   AND his.clock <= ##ENDDATE##;
+`
+
+const mysqlHistoryStr string = `SELECT 
+-- measurement
+replace(replace(CASE
+    WHEN (position('$2' in ite.name) > 0) AND (position('$4' in ite.name) > 0) 
+      THEN replace(replace(ite.name, '$2', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',2), ',',-1)), '$4', SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',-1))
+	WHEN (position('$1' in ite.name) > 0) AND (position('$2' in ite.name) > 0) 
+      THEN replace(replace(ite.name, '$1', SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',1)), '$2', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',2), ',',-1))
+    WHEN (position('$1' in ite.name) > 0) 
+       THEN replace(ite.name, '$1', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',1), ',',-1))
+    WHEN (position('$2' in ite.name) > 0) 
+       THEN replace(ite.name, '$2', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',2), ',',-1))
+	WHEN (position('$3' in ite.name) > 0)
+       THEN replace(ite.name, '$3', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',-2), ',',1))
+    WHEN (position('$1' in ite.name) > 0) AND (position('$3' in ite.name) > 0)
+       THEN replace(replace(ite.name, '$1', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',1), ',',-1)), '$3', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',-2), ',',1))
+    ELSE ite.name
+  END, ',', ''), ' ', '\\ ') 
+-- tags
+|| ',host_name=' || replace(hos.name, ' ', '\\ ')
+|| ',group_name=' || replace(grp.name, ' ', '\\ ')
+|| ',applications=' || ifnull(replace(replace((SELECT GROUP_CONCAT(app.name, ' ')
+    FROM items_applications iap
+    INNER JOIN applications app on app.applicationid = iap.applicationid
+    WHERE iap.itemid = ite.itemid), ' ', '\\ '), ',', ''), 'N.A.')
+|| ' value="' || his.value || '"'
+-- timestamp (in ms)
+|| ' ' || CAST((his.clock * 1000. + his.ns DIV 1000000.) as char) as INLINE
+,  CAST((his.clock * 1000. + his.ns DIV 1000000.) as char) as clock
+FROM history_str his
+INNER JOIN items ite on ite.itemid = his.itemid
+INNER JOIN hosts hos on hos.hostid = ite.hostid
+INNER JOIN hosts_groups hg on hg.hostid = hos.hostid
+INNER JOIN hstgrp grp on grp.groupid = hg.groupid
+WHERE grp.internal=0
+   AND his.clock > ##STARTDATE##
+   AND his.clock <= ##ENDDATE##;
+`
+
+const mysqlHistoryText string = `SELECT 
+-- measurement
+replace(replace(CASE
+    WHEN (position('$2' in ite.name) > 0) AND (position('$4' in ite.name) > 0) 
+      THEN replace(replace(ite.name, '$2', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',2), ',',-1)), '$4', SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',-1))
+	WHEN (position('$1' in ite.name) > 0) AND (position('$2' in ite.name) > 0) 
+      THEN replace(replace(ite.name, '$1', SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',1)), '$2', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',2), ',',-1))
+    WHEN (position('$1' in ite.name) > 0) 
+       THEN replace(ite.name, '$1', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',1), ',',-1))
+    WHEN (position('$2' in ite.name) > 0) 
+       THEN replace(ite.name, '$2', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',2), ',',-1))
+	WHEN (position('$3' in ite.name) > 0)
+       THEN replace(ite.name, '$3', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',-2), ',',1))
+    WHEN (position('$1' in ite.name) > 0) AND (position('$3' in ite.name) > 0)
+       THEN replace(replace(ite.name, '$1', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',1), ',',-1)), '$3', SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(ite.key_, LOCATE('[',ite.key_) + 1, LOCATE(']',ite.key_) - LOCATE('[',ite.key_)-1),',',-2), ',',1))
+    ELSE ite.name
+  END, ',', ''), ' ', '\\ ') 
+-- tags
+|| ',host_name=' || replace(hos.name, ' ', '\\ ')
+|| ',group_name=' || replace(grp.name, ' ', '\\ ')
+|| ',applications=' || ifnull(replace(replace((SELECT GROUP_CONCAT(app.name, ' ')
+    FROM items_applications iap
+    INNER JOIN applications app on app.applicationid = iap.applicationid
+    WHERE iap.itemid = ite.itemid), ' ', '\\ '), ',', ''), 'N.A.')
+|| ' value="' || his.value || '"'
+-- timestamp (in ms)
+|| ' ' || CAST((his.clock * 1000. + his.ns DIV 1000000) as char) as INLINE
+,  CAST((his.clock * 1000. + his.ns DIV 1000000) as char) as clock
+FROM history_text his
 INNER JOIN items ite on ite.itemid = his.itemid
 INNER JOIN hosts hos on hos.hostid = ite.hostid
 INNER JOIN hosts_groups hg on hg.hostid = hos.hostid
